e747dafc9df063e66d01af72cae4675ce4e7febf
Egor Sorokin <znogoud3@gmail.com>
2023-08-29 11:43:41 +0200

    add sidebar, add login/logout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,12 @@
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
+
 import { api } from "~/utils/api";
+
 import "~/styles/globals.css";
 import Head from "next/head";
+import { SideNav } from "~/components/SideNav";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -16,8 +19,8 @@ const MyApp: AppType<{ session: Session | null }> = ({
         <meta name="description" content="This is a Twitter clone" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="container mx-auto flex">
-        {/* <SideNav /> */}
+      <div className="container mx-auto flex items-start sm:pr-4">
+        <SideNav />
         <div className="min-h-screen flex-grow border-x">
           <Component {...pageProps} />
         </div>
