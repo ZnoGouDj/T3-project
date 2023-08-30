64db2d0a0c6f58c38f3fc920bfe7d727b22eeb75
Egor Sorokin <znogoud3@gmail.com>
2023-08-30 22:04:13 +0200

    add 'recent tweets' server logic

---
+++ src/components/InfiniteTweetList.tsx
@@ -0,0 +1,28 @@
+type Tweet = {
+  id: string
+  content: string
+  createdAt: Date
+  likeCount: number
+  likedByMe: boolean
+  user: { id: string; image: string | null; name: string | null }
+}
+
+type InfiniteTweetListProps = {
+  isLoading: boolean
+  isError: boolean
+  hasMore: boolean
+  fetchNewTweets: () => Promise<unknown>
+  tweets?: Tweet[]
+}
+
+export function InfiniteTweetList({ tweets, isError, isLoading, fetchNewTweets, hasMore }: InfiniteTweetListProps) {
+  if (isLoading) return <h1>Loading...</h1>
+  if (isError) return <h1>Error...</h1>
+  if (tweets == null) return null
+
+  if (tweets == null || tweets.length === 0) {
+    return <h2 className="my-4 text-center text-2xl text-gray-500">No Tweets</h2>
+  }
+
+  return <ul></ul>
+}
